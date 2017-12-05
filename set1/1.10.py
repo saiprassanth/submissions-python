67be4473a4a8d5a22c5c205d08f0605836c1a5ff
saiprassanth <33508197+saiprassanth@users.noreply.github.com>
2017-12-05 09:16:03 +0530

    Update 1.10.py

--- set1/1.10.py
+++ set1/1.10.py
@@ -6,7 +6,7 @@ saiprassanth.ramesh@accenture.com
 
 
 '''What would the code above return if the statement x = 25 were replaced by x = -25?
-answer it would become an infinite loop'''
+answer :It would become an infinite loop'''
 
 x = -25
 
@@ -39,3 +39,28 @@ while abs(ans**2 - x) >= epsilon:
 print 'numGuesses =', numGuesses
 
 print ans, 'is close to square root of', x
+
+
+'''cube root of negative and positive numbers
+saiprassanth.ramesh@accenture.com
+5dec2017'''
+z=int(raw_input('enter the number for which cube root is to be found'))
+x =abs(z)
+epsilon = 0.01
+numGuesses = 0
+low = 0.0
+high = max(1.0, x)
+ans = (high + low)/2.0
+while abs(ans**3 - x) >= epsilon:
+  print 'low =', low, 'high =', high, 'ans =', ans
+  numGuesses += 1
+  if ans**3 < x:
+    low = ans
+  else:
+    high = ans
+  ans = (high + low)/2.0
+print 'numGuesses =', numGuesses
+if z<0:
+  print '-',ans, 'is close to cube root of', x
+else:
+  print ans, 'is close to square root of', x
